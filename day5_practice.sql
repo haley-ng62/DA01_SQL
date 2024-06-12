fca398d9a23461c4141da40548f39681fd7a3f89
haley-ng62 <147103810+haley-ng62@users.noreply.github.com>
2024-06-12 16:28:40 -0700

    Update day5_practice.sql

--- day5_practice.sql
+++ day5_practice.sql
@@ -1,1 +1,25 @@
+-- ex1
+SELECT DISTINCT CITY
+FROM STATION
+WHERE ID%2=0;
 
+-- ex2
+SELECT COUNT(*) - COUNT(DISTINCT CITY)
+FROM STATION;
+
+-- ex3
+SELECT CEILING(AVG(SALARY) - AVG(REPLACE(SALARY, '0', '')))
+FROM EMPLOYEES;
+
+-- ex4
+SELECT
+ROUND(CAST(SUM(item_count*order_occurrences)/SUM(order_occurrences) AS DECIMAL),1)
+FROM items_per_order;
+
+-- ex5
+SELECT candidate_id
+FROM candidates
+WHERE skill IN ('Python', 'Tableau', 'PostgreSQL')
+GROUP BY candidate_id
+HAVING COUNT(skill) = 3
+ORDER BY candidate_id;
