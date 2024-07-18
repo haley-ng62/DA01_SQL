167d7b2e19e7f07260ecb0b390cf9aa286e23ff8
haley-ng62 <147103810+haley-ng62@users.noreply.github.com>
2024-07-17 21:57:40 -0700

    Update day10_practice.sql

--- day10_practice.sql
+++ day10_practice.sql
@@ -22,3 +22,20 @@ FROM
   LEFT JOIN texts
     ON emails.email_id = texts.email_id
     AND texts.signup_action = 'Confirmed';
+
+-- ex3
+SELECT
+  b.age_bucket
+  /* time spent sending / total time */
+  , ROUND(
+    SUM(CASE WHEN a.activity_type = 'send' THEN a.time_spent ELSE 0 END)
+      /SUM(a.time_spent) * 100.0
+  , 2) AS send_perc,
+  ROUND(SUM(CASE WHEN a.activity_type = 'open' THEN a.time_spent ELSE 0 END)
+    /SUM(a.time_spent) * 100.0
+  , 2) AS open_perc
+FROM activities AS a
+  INNER JOIN age_breakdown AS b
+  ON a.user_id = b.user_id
+    AND a.activity_type IN ('send', 'open')
+GROUP BY b.age_bucket;
