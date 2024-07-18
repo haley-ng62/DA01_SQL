d8c3f82e27e959cad0455c830bc2bc6bacd3647b
haley-ng62 <147103810+haley-ng62@users.noreply.github.com>
2024-07-17 20:07:25 -0700

    Update day10_practice.sql

--- day10_practice.sql
+++ day10_practice.sql
@@ -1,1 +1,24 @@
+-- ex1
+SELECT
+  COUNTRY.Continent
+  , FLOOR(AVG(CITY.Population))
+    AS avg_city_population
+FROM
+  CITY
+  INNER JOIN COUNTRY
+    ON CITY.CountryCode = COUNTRY.Code
+GROUP BY
+  COUNTRY.Continent;
 
+-- ex2
+SELECT
+  ROUND(
+    CAST(COUNT(texts.email_id) AS numeric)
+    /COUNT(DISTINCT emails.email_id)
+  , 2)
+  AS activation_rate
+FROM
+  emails
+  LEFT JOIN texts
+    ON emails.email_id = texts.email_id
+    AND texts.signup_action = 'Confirmed';
