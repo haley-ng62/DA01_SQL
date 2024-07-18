d864f54c4ef400d6964fe9588183c4339c392abe
haley-ng62 <147103810+haley-ng62@users.noreply.github.com>
2024-07-17 22:06:51 -0700

    Update day10_practice.sql

--- day10_practice.sql
+++ day10_practice.sql
@@ -1,41 +1,38 @@
 -- ex1
 SELECT
-  COUNTRY.Continent
-  , FLOOR(AVG(CITY.Population))
-    AS avg_city_population
-FROM
-  CITY
-  INNER JOIN COUNTRY
-    ON CITY.CountryCode = COUNTRY.Code
+        COUNTRY.Continent,
+        FLOOR(AVG(CITY.Population)) AS avg_city_population
+FROM CITY
+        INNER JOIN COUNTRY
+        ON CITY.CountryCode = COUNTRY.Code
 GROUP BY
-  COUNTRY.Continent;
+        COUNTRY.Continent;
 
 -- ex2
 SELECT
-  ROUND(
-    CAST(COUNT(texts.email_id) AS numeric)
-    /COUNT(DISTINCT emails.email_id)
-  , 2)
-  AS activation_rate
-FROM
-  emails
-  LEFT JOIN texts
-    ON emails.email_id = texts.email_id
-    AND texts.signup_action = 'Confirmed';
+        ROUND(
+          CAST(COUNT(texts.email_id) AS numeric)
+          /COUNT(DISTINCT emails.email_id)
+        , 2)
+        AS activation_rate
+FROM emails
+        LEFT JOIN texts
+        ON emails.email_id = texts.email_id
+          AND texts.signup_action = 'Confirmed';
 
 -- ex3
 SELECT
-  b.age_bucket
-  /* time spent sending / total time */
-  , ROUND(
-    SUM(CASE WHEN a.activity_type = 'send' THEN a.time_spent ELSE 0 END)
-      /SUM(a.time_spent) * 100.0
-  , 2) AS send_perc,
-  ROUND(SUM(CASE WHEN a.activity_type = 'open' THEN a.time_spent ELSE 0 END)
-    /SUM(a.time_spent) * 100.0
-  , 2) AS open_perc
+        b.age_bucket,
+-- time spent sending / total time
+        ROUND(SUM(CASE WHEN a.activity_type = 'send' THEN a.time_spent ELSE 0 END)
+            / SUM(a.time_spent) * 100.0
+          , 2) AS send_perc,
+-- time spent opening / total time
+        ROUND(SUM(CASE WHEN a.activity_type = 'open' THEN a.time_spent ELSE 0 END)
+            / SUM(a.time_spent) * 100.0
+          , 2) AS open_perc
 FROM activities AS a
-  INNER JOIN age_breakdown AS b
-  ON a.user_id = b.user_id
-    AND a.activity_type IN ('send', 'open')
+        INNER JOIN age_breakdown AS b
+        ON a.user_id = b.user_id
+          AND a.activity_type IN ('send', 'open')
 GROUP BY b.age_bucket;
